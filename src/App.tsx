3802cec04754e3c5ebfc7f5dcb305f40db1eecc6
Jeremiah <sinncere520@gmail.com>
2020-06-21 15:38:36 -0400

    x

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
 import React from "react";
-import "./App.css";
+import "./assets/css/main.scss";
 import Navigation from "./components/Navigation";
-import PreviousWork from "./components/PreviousWork";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faJava, faJs, faNodeJs, faVuejs, faPython } from "@fortawesome/free-brands-svg-icons";
 
 const App = () => {
+
   return (
     <div className="container">
+      
       <Navigation />
-      <PreviousWork />
+
+      <div className="header">
+      
+        <div className="introduction">
+          <h1>Hello! I'm Jeremiah</h1>
+          <p>
+            I'm a 20-year old, self-taught software engineer currently located in Philadelphia. 
+            I currently have plans to receive a degree in computer science to further educate my self in this field. 
+            I've been developing different software applications over the last 5 years. 
+            I am fluent in many technologies, I primarily write my frontend applications in React, server side stuff in NodeJS, while using PostgreSQL to handle data management.
+          </p>
+        </div>
+
+        <div className="expertise">
+          <div className="item"><FontAwesomeIcon icon={faNodeJs} /></div>
+          <div className="item"><FontAwesomeIcon icon={faVuejs} /></div>
+          <div className="item"><FontAwesomeIcon icon={faPython} /></div>
+        </div>
+
+      </div>
+
     </div>
   )
 };
--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 const Navigation = () => {
   return (
     <div className="navigation">
-      <ul className="center-nav">
-        <li className="nav-item">Home</li> 
-        <li className="nav-item">Blog</li> 
-        <li className="nav-item">About</li> 
-        <li className="nav-item">Hire Me</li> 
+      <div className="left-nav">
+        <div className="logo"><span>Jeremiah</span> Flores</div>
+        <ul className="navigation-links">
+          <li className="navigation-item active">Projects</li>
+          <li className="navigation-item">Reviews</li>
+          <li className="navigation-item">Hire</li>
+        </ul>
+      </div>
+      <ul className="navigation-social">
+        <li className="navigation-item"><FontAwesomeIcon icon={faTwitter} /> Twitter</li>
+        <li className="navigation-item"><FontAwesomeIcon icon={faGithub} /> Github</li>
       </ul>
     </div>
   )
